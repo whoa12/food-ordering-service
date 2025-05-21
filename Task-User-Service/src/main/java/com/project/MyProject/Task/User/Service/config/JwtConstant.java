b829e4bda67fc194f2c3ed7eb6d5fe6323b4fd86
whoa12 <111991233+whoa12@users.noreply.github.com>
2025-05-21 12:12:31 +0530

    Delete Task-User-Service directory

--- Task-User-Service/src/main/java/com/project/MyProject/Task/User/Service/config/JwtConstant.java
+++
@@ -1,7 +0,0 @@
-package com.project.MyProject.Task.User.Service.config;
-
-public class JwtConstant {
-	public static final String SECRET_KEY= "rhfgwhjfwefbhjdcvbdhvfjhebfhjwbfhjdvfghsfvs";
-	public static final String JWT_HEADER= "Authorization";
-
-}
